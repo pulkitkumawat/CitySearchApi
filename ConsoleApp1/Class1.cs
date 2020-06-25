6aa5a3fe678360af53f617243ca2a76e87760b5d
Pulkit Kumawat <pulkit.kumawat95@gmail.com>
2020-06-25 18:39:54 +0530

    initial commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,106 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleApp1
+//{
+//    public class TestClass<T>
+//    {
+//        T[] obj = new T[5];
+//        int count = 0;
+
+//        public void Add(T obj)
+//        {
+//            if (count < 5)
+//            {
+//                this.obj[count] = obj;
+//                count++;
+//            }
+//        }
+//        public T this[int index]
+//        {
+//            get { return obj[index]; }
+//            set { obj[index] = value; }
+//        }
+//    }
+
+//    public class sort
+//    {
+//        public static string Dir = "asc";
+//        public static string Field = "name";
+//    }
+
+//    public class delExample
+//    {
+
+
+//        public static void m1()
+//        {
+//            Console.WriteLine("m1");
+//        }
+//        public static void m2()
+//        {
+//            Console.WriteLine("m2");
+//        }
+//        public void m3()
+//        {
+//            Console.WriteLine("m3");
+//        }
+//    }
+//    class Class1
+//    {
+//        public delegate void delmethod();
+//        static void Main(string[] args)
+//        {
+//            TestClass<int> obj = new TestClass<int>();
+
+//            obj.Add(1);
+//            obj.Add(2);
+//            obj.Add(3);
+//            obj.Add(4);
+//            obj.Add(5);
+//            obj.Add(6);
+
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine(obj[i]);
+//            }
+
+//            string orderBy = string.Empty;
+
+
+//            orderBy = string.IsNullOrEmpty(orderBy) ? sort.Dir == "desc" ? sort.Field + ' ' + sort.Dir : sort.Field : sort.Dir == "desc" ? orderBy + ", " + sort.Field + ' ' + sort.Dir : orderBy + ", " + sort.Field;
+
+//            Console.WriteLine(orderBy);
+
+//            int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+//            Queue<int> q = new Queue<int>(arr);
+
+//            q.Enqueue(5);
+//            q.Enqueue(4);
+//            q.Enqueue(3);
+//            q.Enqueue(2);
+//            q.Enqueue(1);
+//            q.Dequeue();
+
+//            foreach (var item in q)
+//            {
+//                //Console.WriteLine(q.Peek());
+//                Console.WriteLine(item);
+//            }
+
+//            delmethod del1 = delExample.m1;
+//            delmethod del2 = new delmethod(delExample.m2);
+
+//            delExample obj2 = new delExample();
+//            del2 += obj2.m3;
+
+
+//            del1();
+//            del2();
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string GET(string queryParams)
+        {
+            string baseUrl = "https://singlesearch.alk.com/na/api/search?authToken=FD0602B3D94A3748AD70E9ABA8EEC310&include=Meta";
+            string url = baseUrl + queryParams;
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            bool repeat = false;
+            do
+            {
+                string queryParams = string.Empty;
+                Console.WriteLine("Which country ? Press Y to search in USA");
+                string isUSA = Console.ReadLine();
+                if (isUSA.ToLower() == "y")
+                {
+                    queryParams += "&countries=US";
+                }
+
+                Console.WriteLine("Search in cities only ? Press Y to do that");
+                string isCity = Console.ReadLine();
+                if (isCity.ToLower() == "y")
+                {
+                    queryParams += "&includeOnly=City";
+                }
+
+                Console.WriteLine("Enter the State Code if you want to limit the search to any state");
+                string state = Console.ReadLine();
+                if (!(string.IsNullOrEmpty(state)))
+                {
+                    queryParams += "&states=" + state;
+                }
+
+                Console.WriteLine("Enter the Text you want to search");
+                string query = Console.ReadLine();
+                if (!(string.IsNullOrEmpty(query)))
+                {
+                    queryParams += "&query=" + query;
+                }
+                else
+                {
+                    Console.WriteLine("Quitting!!! query string need to be provided");
+                    System.Environment.Exit(0);
+                }
+
+                JObject response = JObject.Parse(GET(queryParams));
+
+                JArray locationArr = JArray.Parse(response["Locations"].ToString());
+                Console.WriteLine("---------------------------------------------------------------");
+                foreach (var item in locationArr)
+                {
+                    Console.WriteLine(item["ShortString"]);
+                }
+
+                Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("Press Y to see full json response");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+
+                    Console.WriteLine("---------------------------------------------------------------");
+                    Console.WriteLine(response);
+                    Console.WriteLine("---------------------------------------------------------------");
+                }
+
+                Console.WriteLine("Do you want to repeat the Search ?? Press Y , Press any other key to Exit");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    repeat = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    System.Environment.Exit(0);
+                }
+            } while (repeat);
+            
+        }
+    }
+}
